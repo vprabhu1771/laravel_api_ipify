ff07c0c77e2faa1a1897c0ce9da71efcc6e9ecfc
Prabhu <vprabhu1771@gmail.com>
2025-06-04 12:23:53 +0530

    fetch and display user's public IP address using ipify API on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Http;
+
 class HomeController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $response = Http::get('https://api.ipify.org/?format=json');
+
+        if ($response->successful())
+        {
+            $data = $response->json();
+
+            // dd($data);
+
+            // dd($data['ip']);
+
+            return view("home",$data);
+        }
+        else {
+            # code...
+        }
+    }
 }
